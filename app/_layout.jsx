7dd379d6707af5e9dc74f6db9812dd781dc1034a
tamirre <tamirr1999@gmail.com>
2026-01-27 17:03:13 +0200

    added stack layout styles

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,21 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { colors } from '../src/theme/colors';
 
 export default function RootLayout() {
   return (
     <>
+      <StatusBar style="light" />
       <Stack>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="/anime-details/[id]" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="anime-details/[id]"
+          options={{
+            headerTitle: 'Anime Details',
+            headerStyle: { backgroundColor: colors.surface },
+            headerTitleStyle: { color: colors.text },
+          }}
+        />
       </Stack>
     </>
   );
