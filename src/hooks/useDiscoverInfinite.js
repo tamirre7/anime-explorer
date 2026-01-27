2d54fdb838ae2605c9e2899ba5d25e31439a5930
tamirre <tamirr1999@gmail.com>
2026-01-27 23:59:20 +0200

    fixed hooks bug

--- src/hooks/useDiscoverInfinite.js
+++ src/hooks/useDiscoverInfinite.js
@@ -2,15 +2,11 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { api } from '../services/apiClient';
 import { DEFAULT_SORT } from '../constants/animeSort';
 
-export default function useDiscoverInfinite(searchText, sort = DEFAULT_SORT) {
-  const trimmedQuery = searchText?.trim() ?? '';
-
+export default function useDiscoverInfinite(sort = DEFAULT_SORT) {
   return useInfiniteQuery({
-    queryKey: ['discover', trimmedQuery, sort],
-    queryFn: ({ pageParam = 1 }) =>
-      api.discover({ trimmedQuery, sort, pageParam }),
+    queryKey: ['discover', sort],
+    queryFn: ({ pageParam = 1 }) => api.discover({ page: pageParam, sort }),
     initialPageParam: 1,
-    enabled: trimmedQuery.length > 0,
     getNextPageParam: (lastPage) => lastPage?.nextPage ?? undefined,
   });
 }
