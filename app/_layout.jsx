bceefcdf887cd9a9491bbea645f5cffe098f5b82
tamirre <tamirr1999@gmail.com>
2026-01-27 22:40:20 +0200

    created query cliend and wrapping the initial layout with QueryClientProvider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { colors } from '../src/theme/colors';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <StatusBar style="light" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -17,6 +20,6 @@ export default function RootLayout() {
           }}
         />
       </Stack>
-    </>
+    </QueryClientProvider>
   );
 }
