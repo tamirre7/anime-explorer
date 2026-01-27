c44e9e888dd01809377db8f8964fa756f9cf5ea3
tamirre <tamirr1999@gmail.com>
2026-01-27 18:28:01 +0200

    added anime details and summary factory using JSDOC models

---
+++ src/models/animeDetails.js
@@ -0,0 +1,61 @@
+// @ts-check
+
+/**
+ * @typedef {object} AnimeDetails
+ * @property {number} id
+ * @property {string} title
+ * @property {number|null} episodes
+ * @property {number|null} score
+ * @property {string[]} genres
+ * @property {string} synopsis
+ * @property {string} imageUrl
+ * @property {string} rating
+ * @property {string} yearsOfAiring
+ */
+
+/**
+ * @typedef {object} AnimeDetailsInput
+ * @property {number} id
+ * @property {string} title
+ * @property {number|null} [episodes]
+ * @property {number|null} [score]
+ * @property {string[]} [genres]
+ * @property {string} [synopsis]
+ * @property {string} [imageUrl]
+ * @property {string} [rating]
+ * @property {string} [yearsOfAiring]
+ */
+
+/**
+ * @param {AnimeDetailsInput} input
+ * @returns {AnimeDetails}
+ */
+export function createAnimeDetails(input) {
+  const {
+    id,
+    title,
+    episodes = null,
+    score = null,
+    genres = [],
+    synopsis = '',
+    imageUrl = '',
+    rating = '',
+    yearsOfAiring = '',
+  } = input;
+
+  if (typeof id !== 'number') throw new TypeError('id must be a number');
+  if (typeof title !== 'string' || !title)
+    throw new TypeError('title is required');
+
+  return {
+    id,
+    title,
+    episodes,
+    score,
+    genres,
+    synopsis,
+    imageUrl,
+    rating,
+    yearsOfAiring,
+  };
+}
