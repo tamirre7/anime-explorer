8f159c0d6f96ff8c5382696a4d7653593b30c47f
tamirre <tamirr1999@gmail.com>
2026-01-27 23:45:42 +0200

    implemented the search and the discover hooks

--- src/hooks/useDiscoverInfinite.js
+++ src/hooks/useDiscoverInfinite.js
@@ -1,6 +1,16 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { api } from '../services/apiClient';
+import { DEFAULT_SORT } from '../constants/animeSort';
 
-export default function useDiscoverInfinite(page = 1) {
-  return;
+export default function useDiscoverInfinite(searchText, sort = DEFAULT_SORT) {
+  const trimmedQuery = searchText?.trim() ?? '';
+
+  return useInfiniteQuery({
+    queryKey: ['discover', trimmedQuery, sort],
+    queryFn: ({ pageParam = 1 }) =>
+      api.discover({ trimmedQuery, sort, pageParam }),
+    initialPageParam: 1,
+    enabled: trimmedQuery.length > 0,
+    getNextPageParam: (lastPage) => lastPage?.nextPage ?? undefined,
+  });
 }
