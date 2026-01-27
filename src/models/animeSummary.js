823648e72096111ef4ba668f2e7744087b00e792
tamirre <tamirr1999@gmail.com>
2026-01-27 20:33:06 +0200

    added airing years to anime summary

--- src/models/animeSummary.js
+++ src/models/animeSummary.js
@@ -7,6 +7,7 @@
  * @property {number|null} score
  * @property {string[]} genres
  * @property {string} imageUrl
+ * @property {string} airingYears
  */
 
 /**
@@ -16,6 +17,7 @@
  * @property {number|null} [score]
  * @property {string[]} [genres]
  * @property {string} [imageUrl]
+ * @property {string} [airingYears]
  */
 
 /**
@@ -23,7 +25,14 @@
  * @returns {AnimeDetails}
  */
 export function createAnimeSummary(input) {
-  const { id, title, score = null, genres = [], imageUrl = '' } = input;
+  const {
+    id,
+    title,
+    score = null,
+    genres = [],
+    imageUrl = '',
+    airingYears = '',
+  } = input;
 
   if (typeof id !== 'number') throw new TypeError('id must be a number');
   if (typeof title !== 'string' || !title)
@@ -35,5 +44,6 @@ export function createAnimeSummary(input) {
     score,
     genres,
     imageUrl,
+    airingYears,
   };
 }
