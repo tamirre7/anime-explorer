aa2c09b92ca73e29f8688f844f9a45f41df2b673
tamirre <tamirr1999@gmail.com>
2026-01-28 13:34:44 +0200

    implemented initial animne grid

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -1,1 +1,1 @@
-export default function AnimeCard() {}
+export default function AnimeCard({ anime }) {}
--- src/components/AnimeGrid/AnimeGrid.jsx
+++ src/components/AnimeGrid/AnimeGrid.jsx
@@ -1,1 +1,14 @@
-export default function AnimeGrid() {}
+import { FlatList } from 'react-native';
+import AnimeCard from '../AnimeCard/AnimeCard';
+
+export default function AnimeGrid({ visibleSeries }) {
+  return (
+    <>
+      <FlatList
+        data={visibleSeries}
+        renderItem={({ series }) => <AnimeCard anime={series} />}
+        keyExtractor={(series) => series.id}
+      />
+    </>
+  );
+}
